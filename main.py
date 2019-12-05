c9c9f73c0e75992fe2ba3b4649192908465336d4
Koki Miura <memjoalty@yahoo.co.jp>
2019-12-06 02:05:51 +0900

    Out headers

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def lambda_handler(event=None, context=None):
 # /clova に対してのPOSTリクエストを受け付けるサーバーを立てる
 @app.route('/clova', methods=['POST'])
 def my_service():
+    print(request.headers)
     body_dict = clova.route(body=request.data, header=request.headers)
     response = jsonify(body_dict)
     response.headers['Content-Type'] = 'application/json;charset-UTF-8'
